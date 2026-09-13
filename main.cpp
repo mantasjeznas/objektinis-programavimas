9c5170206350e62c882d8dc32377d677ec644a5c
Mantas Jeznas <mantasjeznas2004@gmail.com>
2026-09-13 13:53:11 +0300

    pridėta mediana ir galutinio balo pasirinkimas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 using std::string;
 using std::vector;
 using std::cout;
@@ -13,9 +14,10 @@ struct studentas{
     int exam;
 };
 
-void printas(const studentas &A);
+void printas(const studentas &A, int pasirinkimas);
 double vidurkis(const studentas &A);
-double galutinis(const studentas &A);
+double mediana(const studentas &A);
+double galutinis(const studentas &A, bool ar_mediana);
 
 int main(){
     int k;
@@ -42,10 +44,29 @@ int main(){
         A.paz.clear();
     }
 
-    cout << std::left << std::setw(15) <<"Pavarde"<< std::left << std::setw(15) <<"Vardas"
-    <<"Galutinis (Vid.)\n"<< "-----------------------------------------------\n";
+    cout << "\nKaip skaiciuoti galutini bala?\n"
+    <<"1 - pagal vidurki\n"
+    <<"2 - pagal mediana\n"
+    <<"3 - abu\n";
+    int pasirinkimas = 0;
+    while (pasirinkimas != 1 && pasirinkimas != 2 && pasirinkimas != 3) {
+        cout << "Pasirinkimas: \n";
+        cin >> pasirinkimas;
+        if (pasirinkimas != 1 && pasirinkimas != 2 && pasirinkimas != 3){
+            cout << "neteisinga ivestis\n";
+        }
+}
 
-    for (studentas &B:grupe) printas(B);
+    cout << std::left << std::setw(15) <<"Pavarde"<< std::left << std::setw(15) <<"Vardas";
+    if (pasirinkimas == 1) cout << "Galutinis (Vid.)\n";
+    else if (pasirinkimas == 2) cout << "Galutinis (Med.)\n";
+    else cout << std::left << std::setw(18) << "Galutinis (Vid.)" << "Galutinis (Med.)\n";
+    
+    int linijos_ilgis = 47;
+    if (pasirinkimas == 3) linijos_ilgis = 65;
+    cout << string(linijos_ilgis, '-') << "\n";
+
+    for (studentas &B:grupe) printas(B, pasirinkimas);
 
     return 0;
 }
@@ -59,12 +80,32 @@ double vidurkis(const studentas &A) {
     return suma / A.paz.size();
 }
 
-double galutinis(const studentas &A) {
+double mediana(const studentas &A) {
+    if (A.paz.empty()) return 0;
+    vector<int> laikinas = A.paz;
+    std::sort(laikinas.begin(), laikinas.end());
+    int n = laikinas.size();
+    if (n % 2 == 1) return laikinas[n / 2]; // grazina vidurine reiksme, nes sveikoji dalyba
+    return (laikinas[n / 2 - 1] + laikinas[n / 2]) / 2.0;
+}
+
+double galutinis(const studentas &A, bool ar_mediana) {
+    if (ar_mediana == true){
+    return 0.4 * mediana(A) + 0.6 * A.exam;
+    }
     return 0.4 * vidurkis(A) + 0.6 * A.exam;
 }
 
-void printas(const studentas &A){
+void printas(const studentas &A, int pasirinkimas){
     cout << std::left << std::setw(15) << A.pavarde
     << std::left << std::setw(15) << A.vardas
-    << std::fixed << std::setprecision(2) << galutinis(A) << "\n";
+    << std::fixed << std::setprecision(2);
+
+    if (pasirinkimas == 1) {
+        cout << galutinis(A, false) << "\n";
+    } else if (pasirinkimas == 2) {
+        cout << galutinis(A, true) << "\n";
+    } else {
+        cout << std::left << std::setw(18) << galutinis(A, false) << galutinis(A, true) << "\n";
+    }
 }
